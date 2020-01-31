a19c56b7a0e9ce919e741abd3b3fc2300d6b44e9
hashikapur <58193820+hashikapur@users.noreply.github.com>
2020-01-30 23:50:03 -0600

    Update hw1.c

--- hw1.c
+++ hw1.c
@@ -1,39 +1,39 @@
 #include <stdio.h>
 #include "hw1.h"
 
-static void I_have_written(){}
+static int I_have_written(){}
 static int the_code[3] = {0, 1, 2};
 static void that_you_needed(){}
 static void and_which(){}
 
 void sides_and(){}
 void Forgive_me(){}
+
 int they_are_arbitrary; 
-int so_random[2] = {1, 2};
+int so_random[2] = {10, 11};
+
 void and_so_varied(){}
 
 int main(int argc,char* argv[]){
-
-  static int to_compile[1] = {3};
-  static int has_a_bunch_of[1] = {4};
-  static int ridiculous[1] = {5};
+  
+  static int to_compile[1] = {4};
+  static int has_a_bunch_of[1] = {5};
+  static int ridiculous[1] = {6};
   static int symbols[2];
- 
-  printf("ango8");
+
+  printf("hkapur2");
 
   I_have_written();
   that_you_needed();
-
   and_which();
-
   Forgive_me();
   and_so_varied();
 
   to_compile[1] = to_compile[1];
-  the_code[3] = the_code[2];
-  has_a_bunch_of[1] = has_a_bunch_of[1];
-  ridiculous[1] = ridiculous[1];
-  symbols[2] = symbols[2];
+  the_code[10] = the_code[10];
+  has_a_bunch_of[11] = has_a_bunch_of[11];
+  ridiculous[12] = ridiculous[12];
+  symbols[13] = symbols[13];
 
   return 0;
 }
