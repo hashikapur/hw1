bb1b60187a13a44b6d2a7e4a8aca0df7a223953a
hashikapur <58193820+hashikapur@users.noreply.github.com>
2020-01-30 23:44:04 -0600

    Update hw1.c

--- hw1.c
+++ hw1.c
@@ -4,20 +4,17 @@
 static void I_have_written(){}
 static int the_code[3] = {0, 1, 2};
 static void that_you_needed(){}
-
 static void and_which(){}
 
-// global variables - removed "static"
 void sides_and(){}
-
 void Forgive_me(){}
-int they_are_arbitrary; // uninitialized global variable
+int they_are_arbitrary; 
 int so_random[2] = {1, 2};
 void and_so_varied(){}
 
 int main(int argc,char* argv[]){
-  // array in main will give .# in Name
-  static int to_compile[1] = {0};
+
+  static int to_compile[1] = {2};
   static int has_a_bunch_of[1] = {1};
   static int ridiculous[1] = {2};
   static int symbols[2];
